92f1fc3962dc0244a39865b3f1bdc5c54cec81b3
Anyalewechi Onyinyechi <106473945+0nyin@users.noreply.github.com>
2023-12-14 12:19:11 +0100

    new update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import wallet from '../assets/wallet.png'
 import galitech from '../assets/galitech.png'
-import van from '../assets/van.png'
 import pay from '../assets/pay.png'
+import van from '../assets/van.png'
 import color from '../assets/color.png'
 import airbnb from '../assets/air.png'
 
@@ -48,29 +48,10 @@ function Work() {
                   React js Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://galitech-web.vercel.app' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href='https://github.com/0nyin/Galitech-web' target="_blank">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div style={{backgroundImage: `url(${van})`}} 
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-            {/* hover effect */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                  React js Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='#'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href='https://github.com/0nyin/Router-project' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
@@ -90,12 +71,13 @@ function Work() {
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  {/* <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button> */}
                 </a>
               </div>
             </div>
           </div>
-          <div style={{backgroundImage: `url(${color})`}} 
+
+          <div style={{backgroundImage: `url(${van})`}} 
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* hover effect */}
@@ -105,11 +87,33 @@ function Work() {
               </span>
               <div className='pt-8 text-center'>
                 <a href='#'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  
                 </a>
-                <a href='#'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                <a href='https://github.com/0nyin/Router-project' target="_blank">
+                  
                 </a>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+              </div>
+            </div>
+          </div>
+
+          
+          <div style={{backgroundImage: `url(${color})`}} 
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover effect */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                  React js Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/#'>
+                </a>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                <a href='/#'>
+                </a>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
               </div>
             </div>
           </div>
@@ -124,11 +128,13 @@ function Work() {
               </span>
               <div className='pt-8 text-center'>
                 <a href='/#'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  
                 </a>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 <a href='/#'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+          
                 </a>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
               </div>
             </div>
           </div>
